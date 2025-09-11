7f933c4fcafedf13344635080fd9df3d9a16ca7a
Muchakazi Webster <muchakaziwebster.it@gmail.com>
2025-09-11 20:37:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: ['localhost'],
-    unoptimized: true
-  }
+    domains: ['via.placeholder.com', 'picsum.photos'], // Add domains for external images
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 }
 
 module.exports = nextConfig
