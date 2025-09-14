d0beeaecf530a6626659ad9a776288d6f48013fe
Muchakazi Webster <muchakaziwebster.it@gmail.com>
2025-09-14 08:00:28 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/Header'
+import HeroCards from '@/components/HeroCards'
+import ToolsGrid from '@/components/ToolsGrid'
+import GallerySection from '@/components/GallerySection'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50 dark:bg-gray-900">
+      <Header />
+      <div className="max-w-7xl mx-auto">
+        <HeroCards />
+        <ToolsGrid />
+        <GallerySection />
+      </div>
+    </main>
+  )
+}
